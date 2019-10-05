dce71ca2f25d0be114af4d0c6a7f8db5c859381b
Dauren <dauren.asir@gmail.com>
2019-10-06 00:44:53 +0600

    task6

---
+++ task6/core.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+type writerWithLog struct {
+	target   io.Writer
+	total    uint64
+	progress uint64
+}
+
+func (o *writerWithLog) Write(p []byte) (int, error) {
+	n, err := o.target.Write(p)
+	if err == nil && n > 0 {
+		o.progress += uint64(n)
+		fmt.Printf("\rProgress: %d%% ", (o.progress*100)/o.total)
+	}
+	return n, err
+}
+
+func (o *writerWithLog) FinishProgress() {
+	fmt.Println()
+}
+
+/*
+FileCopy is function for copy files from src to dst.
+offset - is offset in src before copy
+limit - is limit in bytes length
+*/
+func FileCopy(src, dst string, offset, limit int64) error {
+	var err error
+
+	var srcFile *os.File
+	srcFile, err = os.Open(src)
+	if err != nil {
+		log.Printf("Fail to open source file: %s\n", err.Error())
+		return err
+	}
+	defer srcFile.Close()
+
+	var srcFileSize uint64
+
+	// stat of src
+	var srcFileInfo os.FileInfo
+	srcFileInfo, err = srcFile.Stat()
+	if err == nil {
+		srcFileSize = uint64(srcFileInfo.Size())
+	} else {
+		log.Printf("Fail to get stat of source file: %s\n", err.Error())
+	}
+
+	var dstFile *os.File
+	dstFile, err = os.Create(dst)
+	if err != nil {
+		log.Printf("Fail to open destination file: %s\n", err.Error())
+		return err
+	}
+	defer dstFile.Close()
+
+	dstWrapper := &writerWithLog{target: dstFile, total: srcFileSize}
+
+	if offset > 0 {
+		_, err = srcFile.Seek(offset, io.SeekStart)
+		if err != nil {
+			log.Printf("Fail to seek source file: %s\n", err.Error())
+			return err
+		}
+	}
+
+	if limit <= 0 {
+		_, err = io.Copy(dstWrapper, srcFile)
+	} else {
+		_, err = io.CopyN(dstWrapper, srcFile, limit)
+		if err == io.EOF { // if out of the file size
+			err = nil
+		}
+	}
+	if err != nil {
+		log.Printf("Fail to copy file: %s\n", err.Error())
+		return err
+	}
+
+	dstWrapper.FinishProgress()
+
+	return nil
+}
---
+++ task6/task6.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+var (
+	argFrom   string
+	argTo     string
+	argOffset int64
+	argLimit  int64
+)
+
+func init() {
+	flag.StringVar(&argFrom, "from", "", "file to read from (required)")
+	flag.StringVar(&argTo, "to", "", "file to write to (required)")
+	flag.Int64Var(&argOffset, "offset", 0, "bytes to omit in source file, before copy (default 0)")
+	flag.Int64Var(&argLimit, "limit", 0, "bytes count to copy (default 0, that mean 'no limit')")
+}
+
+func main() {
+	flag.Parse()
+
+	// Validation arguments
+	if argFrom == "" || argTo == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+	if argOffset < 0 {
+		fmt.Println("offset must be a positive number")
+		flag.Usage()
+		os.Exit(1)
+	}
+	if argLimit < 0 {
+		fmt.Println("limit must be a positive number")
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	err := FileCopy(argFrom, argTo, argOffset, argLimit)
+	if err != nil {
+		os.Exit(1)
+	}
+
+	os.Exit(0)
+}
